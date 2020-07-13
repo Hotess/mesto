8ac7083464c9084001aa67d06a0d9513a2032922
Pavel Opolovnin <hote_95@bk.ru>
2020-07-14 00:18:07 +0300

    subject: complete

--- scripts/script.js
+++ scripts/script.js
@@ -3,50 +3,33 @@ let popup = document.querySelector('.popup');
 let popupForm = document.querySelector('.popup__form');
 let popupClose = document.querySelector('.popup__close');
 let profileInfo = document.querySelector('.profile__info');
+let addButton = document.querySelector('.profile__add-button');
+let elements = document.querySelector('.elements');
+let element = document.querySelector('.element');
+let popupName = document.querySelector('.popup__item_name');
+let popupAboutU = document.querySelector('.popup__item_about-u');
+let profileName = document.querySelector('.profile__name');
+let profileWorking = document.querySelector('.profile__working');
 
-editButton.addEventListener('click', function() {
+function openPopup() {
 	popup.classList.toggle('popup_opened');
-	
-});
+	popupName.value = profileName.textContent;
+	popupAboutU.value = profileWorking.textContent;
+}
 
 function closePopup() {
 	popup.classList.remove('popup_opened');
 }
 
-popupClose.addEventListener('click', closePopup);
-
-popupForm.addEventListener('submit', function(event) {
+function editPopup() {
 	event.preventDefault();
 	
-	for (let i = 0; i < popupForm.elements.length; i++) {
-		let profileInfoElements = [];
-		
-			for (let elem of profileInfo.children) {
-				if (!(elem.nodeName == "IMG")) {
-					profileInfoElements.push(elem);
-				}
-			}
-		
-		if (popupForm.elements[i].type == "text") {
-			profileInfoElements[i].textContent = popupForm.elements[i].value; 
-			
-			if (profileInfoElements[i].nodeName == "H2" && profileInfoElements[i].textContent.length > 20) {		
-				profileInfoElements[i].textContent = profileInfoElements[i].textContent.slice(0, 17);
-			}
-			
-			if (profileInfoElements[i].nodeName == "P" && profileInfoElements[i].textContent.length > 20) {
-				profileInfoElements[i].textContent = profileInfoElements[i].textContent.slice(0, 60);
-			}
-		}
-	}
+	profileName.textContent = popupName.value;
+	profileWorking.textContent = popupAboutU.value;
 	
 	closePopup();
-});
+}
 
-let addButton = document.querySelector('.profile__add-button');
-let elements = document.querySelector('.elements');
-let element = document.querySelector('.element');
-
-addButton.addEventListener('click', () => {
-	elements.insertAdjacentHTML('afterBegin', element.outerHTML);
-});
+editButton.addEventListener('click', openPopup);
+popupForm.addEventListener('submit', editPopup);
+popupClose.addEventListener('click', closePopup);
